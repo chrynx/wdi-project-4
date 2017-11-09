bd338f1498c7c292d94d77b97ab3db475d6a5c0d
chrynx <ralphmadriaga@gmail.com>
2017-11-09 08:45:43 +0000

    changed name to just Datr

--- src/components/landingpage/LandingPageHeader.js
+++ src/components/landingpage/LandingPageHeader.js
@@ -4,7 +4,7 @@ import '../../scss/components/landingpage/LandingPageHeader.scss';
 const LandingPage = () => {
   return (
     <section className="LandingPageHeader">
-      <p>Online Datr</p>
+      <p>Datr</p>
       <p></p>
       <p></p>
       <p></p>
